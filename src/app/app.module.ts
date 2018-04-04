047273a483f1617ed093b21451c518ee57a83059
Nicolas.hedoire <nicolashedoire@hotmail.com>
2018-04-04 16:34:48 +0200

    add google maps autocomplete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AgmCoreModule } from '@agm/core';
 
 import {
   SocialLoginModule,
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
 import { MembersComponent } from './members/members.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Configs
 export function getAuthServiceConfigs() {
@@ -23,7 +25,9 @@ export function getAuthServiceConfigs() {
     },
     {
       id: GoogleLoginProvider.PROVIDER_ID,
-      provider: new GoogleLoginProvider('142771585417-2d87tilqo36mivtmomhnjd2au29vo42v.apps.googleusercontent.com')
+      provider: new GoogleLoginProvider(
+        '142771585417-2d87tilqo36mivtmomhnjd2au29vo42v.apps.googleusercontent.com'
+      )
     }
   ]);
   return config;
@@ -36,7 +40,17 @@ export function getAuthServiceConfigs() {
     MembersComponent,
     LoginComponent
   ],
-  imports: [BrowserModule, NgbModule.forRoot(), SocialLoginModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule.forRoot(),
+    SocialLoginModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBh3vbT19X2EIzKfYlOYnfBET22Usjro_c',
+      libraries: ['places']
+    })
+  ],
   providers: [
     {
       provide: AuthServiceConfig,

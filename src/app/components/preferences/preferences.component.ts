4be1767321d4c9cb26245eba68ec6608bf292bae
Nicolas.hedoire <nicolashedoire@hotmail.com>
2018-04-19 16:38:33 +0200

    add drinks in preferences

--- src/app/components/preferences/preferences.component.ts
+++ src/app/components/preferences/preferences.component.ts
@@ -5,11 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './preferences.component.html',
   styleUrls: ['./preferences.component.css']
 })
-export class PreferencesComponent implements OnInit {
+export class PreferencesComponent {
+  alcool = '';
+  alcoolsImg = [
+    {image: '/assets/img/wine.png', name: 'Vin'},
+    {image: '/assets/img/whisky.png', name: 'Whisky'},
+    {image: '/assets/img/rhum.png', name: 'Rhum'},
+    {image: '/assets/img/beer.png', name: 'Bière'},
+    {image: '/assets/img/soda.png', name: 'Soda'},
+    {image: '/assets/img/cafe.png', name: 'Café'},
+    {image: '/assets/img/cocktail.png', name: 'Cocktail'},
+    {image: '/assets/img/martini.png', name: 'Martini'}
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
+  alcoolOnChange(value: string) {
+    this.alcool = this.alcoolsImg[value];
+    console.log(value);
   }
-
 }

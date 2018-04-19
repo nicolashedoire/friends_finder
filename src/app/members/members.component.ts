13838ccec8d129c7cb7589dc2730ec4d89d1d9c0
Nicolas.hedoire <nicolashedoire@hotmail.com>
2018-04-19 09:52:14 +0200

    add switch profile previousMember and nextMember

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -6,10 +6,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./members.component.css']
 })
 export class MembersComponent implements OnInit {
+  members = [
+    {
+      name: 'Julie',
+      image: 'https://randomuser.me/api/portraits/women/79.jpg'
+    },
+    { name: 'Caro', image: 'https://randomuser.me/api/portraits/women/80.jpg' },
+    {
+      name: 'Claire',
+      image: 'https://randomuser.me/api/portraits/women/81.jpg'
+    },
+    {
+      name: 'Belinda',
+      image: 'https://randomuser.me/api/portraits/women/82.jpg'
+    },
+    {
+      name: 'Alice',
+      image: 'https://randomuser.me/api/portraits/women/83.jpg'
+    },
+    {
+      name: 'Charlotte',
+      image: 'https://randomuser.me/api/portraits/women/84.jpg'
+    }
+  ];
 
-  constructor() { }
+  member: any;
+  count = 0;
 
-  ngOnInit() {
+  constructor() {
+    this.member = this.members[0];
   }
 
+  ngOnInit() {}
+
+  previousMember(): void {
+    if (this.count === 0) {
+      return;
+    }
+
+    this.count--;
+    this.member = this.members[this.count];
+  }
+
+  nextMember(): void {
+    if (this.count === this.members.length) {
+      return;
+    }
+
+    this.count++;
+    this.member = this.members[this.count];
+  }
 }

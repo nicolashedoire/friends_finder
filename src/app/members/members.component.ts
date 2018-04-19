81cdb8bdc57f47e3f861390fcba23ed95e03a1f3
Nicolas.hedoire <nicolashedoire@hotmail.com>
2018-04-19 09:57:43 +0200

    fix error on nextMember

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -8,23 +8,32 @@ import { Component, OnInit } from '@angular/core';
 export class MembersComponent implements OnInit {
   members = [
     {
+      id: 1,
       name: 'Julie',
       image: 'https://randomuser.me/api/portraits/women/79.jpg'
     },
-    { name: 'Caro', image: 'https://randomuser.me/api/portraits/women/80.jpg' },
     {
+      id: 2,
+      name: 'Caro',
+      image: 'https://randomuser.me/api/portraits/women/80.jpg'
+    },
+    {
+      id: 3,
       name: 'Claire',
       image: 'https://randomuser.me/api/portraits/women/81.jpg'
     },
     {
+      id: 4,
       name: 'Belinda',
       image: 'https://randomuser.me/api/portraits/women/82.jpg'
     },
     {
+      id: 5,
       name: 'Alice',
       image: 'https://randomuser.me/api/portraits/women/83.jpg'
     },
     {
+      id: 6,
       name: 'Charlotte',
       image: 'https://randomuser.me/api/portraits/women/84.jpg'
     }
@@ -49,11 +58,16 @@ export class MembersComponent implements OnInit {
   }
 
   nextMember(): void {
-    if (this.count === this.members.length) {
+    if (this.count === this.members.length - 1) {
       return;
     }
 
     this.count++;
     this.member = this.members[this.count];
+  }
+
+  joinMember(id: string): void {
+
+    console.log(id);
   }
 }

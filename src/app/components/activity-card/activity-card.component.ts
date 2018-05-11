b1e20e468759481affbe30f7ba0a6d39c74e33a0
Nicolas.hedoire <nicolashedoire@hotmail.com>
2018-05-11 15:57:10 +0200

    add output on diffent state view

--- src/app/components/activity-card/activity-card.component.ts
+++ src/app/components/activity-card/activity-card.component.ts
@@ -1,22 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate, keyframes } from '@angular/core';
 import { AuthentificationService } from '../../shared/security/auth.service';
 
 @Component({
   selector: 'app-activity-card',
   templateUrl: './activity-card.component.html',
-  styleUrls: ['./activity-card.component.css']
+  styleUrls: ['./activity-card.component.css'] ,
+  animations: [
+    trigger('showView', [
+      state('inactive', style({
+        display: 'none'
+      })),
+      state('active', style({
+        display: 'block'
+      }))
+    ])
+  ]
 })
 export class ActivityCardComponent implements OnInit {
 
   userData = null;
+  state = 'inactive';
+  stateMenu = 'active';
 
   constructor(public authService: AuthentificationService) {
     if (this.authService.isAuthenticated()) {
       this.userData = this.authService.decodeToken();
-
-      console.log(this.userData);
     }
   }
 
   ngOnInit() {}
+
+  showView(item) {
+    this.state = (this.state === 'inactive' ? 'active' : 'inactive');
+    this.stateMenu = (this.stateMenu === 'active' ? 'inactive' : 'active');
+  }
+
+
+  handleOriginView() {
+    this.state = (this.state === 'inactive' ? 'active' : 'inactive');
+    this.stateMenu = (this.stateMenu === 'active' ? 'inactive' : 'active');
+  }
 }
---
+++ src/app/components/bar-list/bar-list.component.ts
@@ -0,0 +1,100 @@
+import {
+  Component,
+  OnInit,
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+  keyframes,
+  Output,
+  EventEmitter
+} from '@angular/core';
+import { PlaceService } from '../../services/place.service';
+
+
+@Component({
+  selector: 'app-bar-list',
+  templateUrl: './bar-list.component.html',
+  styleUrls: ['./bar-list.component.css'],
+  animations: [
+    trigger('showView', [
+      state(
+        'inactive',
+        style({
+          display: 'none'
+        })
+      ),
+      state(
+        'active',
+        style({
+          display: 'block'
+        })
+      )
+    ])
+  ]
+})
+export class BarListComponent implements OnInit {
+
+  @Output() setOriginView = new EventEmitter();
+
+  state = 'inactive';
+  stateMenu = 'active';
+  places = [];
+
+  time = this.updateTime();
+  activityTime: string;
+
+  constructor(private placeService: PlaceService) {
+    placeService.getAll().subscribe(response => {
+      this.places = response;
+    });
+  }
+
+  ngOnInit() {}
+
+  selectPlace(id: string) {
+    console.log(id);
+    this.state = this.state === 'inactive' ? 'active' : 'inactive';
+    this.stateMenu = (this.stateMenu === 'active' ? 'inactive' : 'active');
+    console.log(this.state);
+  }
+
+  updateTime() {
+    const d = new Date();
+    const time = {
+      hour: ('00' + d.getHours()).slice(-2).toString(),
+      minute: ('00' + d.getMinutes()).slice(-2).toString()
+    };
+    return time;
+  }
+
+  formatTime(event) {
+    const time =
+      this.minTwoDigits(event.hour) + ':' + this.minTwoDigits(event.minute);
+
+    return time;
+  }
+
+  minTwoDigits(n) {
+    return (n < 10 ? '0' : '') + n;
+  }
+
+  onTimeChange(event) {
+    if (event !== null) {
+      this.activityTime = this.formatTime(event);
+    }
+  }
+
+  returnMenu() {
+
+    if (this.state === 'active') {
+      this.state = 'inactive';
+      this.stateMenu = (this.stateMenu === 'active' ? 'inactive' : 'active');
+      return;
+    }
+
+    this.setOriginView.emit();
+  }
+
+}

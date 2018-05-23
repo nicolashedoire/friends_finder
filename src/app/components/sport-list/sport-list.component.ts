6bb6aa635771bbf9dc855cf6ebe9745dfa65ed40
Nicolas.hedoire <nicolashedoire@hotmail.com>
2018-05-23 11:52:43 +0200

    add create sport and getAll in sport list component

--- src/app/components/sport-list/sport-list.component.ts
+++ src/app/components/sport-list/sport-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule, Routes, Router, ActivatedRoute } from '@angular/router';
+import { SportService } from '../../services/sport.service';
 
 @Component({
   selector: 'app-sport-list',
@@ -8,17 +9,26 @@ import { RouterModule, Routes, Router, ActivatedRoute } from '@angular/router';
 })
 export class SportListComponent implements OnInit {
 
+  sports = [];
+  sport: string;
   choice: string;
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) { 
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private sportService: SportService) {
 
     this.choice = this.activatedRoute.snapshot.paramMap.get('choice');
+
+    this.sportService.getAll().subscribe(sports => {
+      this.sports = sports;
+    });
   }
 
   ngOnInit() {
   }
 
   addSport() {
-
+    this.sportService.add(this.sport).subscribe(Response => {
+        // TODO ADD SUCCESS MESSAGE
+        this.router.navigate(['/activity/' + this.choice]);
+    });
   }
 }
